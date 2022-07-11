2c28ba9d2fcb06ee41f66c396f9ab6cc9c1678b7
DARK UMAR <104460597+pindiboy404@users.noreply.github.com>
2022-07-11 23:57:35 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,22 +3,22 @@
 //or copy the codes/script,
 //pls give credit
 //no credit? i will take action immediately
-//© 2022 Xeon Bot Inc. Cheems Bot MD
-//Thank you to Lord Buddha, Family and Myself
+//© 2022 Umar Bot Inc. Devils Bot MD
+//Thank you to Umar Devil, Family and Myself
 //════════════════════════════//
 //recode kar ke youtube pe upload kar rhe hai ya
 //codes copy kar ke apne script me dal rhe
 //hai to, description me xeon ka yt channel
 // ka link paste kr dena as a cradit or github 
 //repo me bhi tag kardena baki jo
-//bhi karna hai apki marzi, thank you!🦄
+//bhi karna hai apki marzi, thank you!😈
 //════════════════════════════//
 //If you recode and uploading on your channel
 //or copy pasting the codes in ur script, 
 //i give permission to do as long as you
-//put Xeons youtube channel link in the video
+//put Umar youtube channel link in the video
 //description and tag me on githuh repo, 
-//thank you🦄
+//thank you😈
 //════════════════════════════//
 
 const fs = require('fs')
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
+global.vcardowner = [923165123719'] //ur owner number
+global.ownername = "UMAR DEVIL" //ur owner name
+global.ytname = "YT: Umar" //ur yt chanel name
+global.socialm = "GitHub: Umar" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
+global.owner = [923165123719']
+global.ownertag = '923165123719' //ur tag number
+global.botname = 'UMAR DEVIL MD V4' //ur bot name
+global.linkz = "https://www.youtube.com/c/UMARMODS" //your theme url which will be displayed on whatsapp
 global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.botscript = 'https://github.com/LanLelo🤣😅/' //script link
+global.reactmoji = "👿" //ur menu react emoji
+global.themeemoji = "👿" //ur theme emoji
+global.packname = "It's Me Umar Devil\n\n\n\n\n\n\n\n\n\n\nUMAR DEVIL +923165123719 😈😈😈" //ur sticker watermark packname
+global.author = "UMAR DEVIL 👿" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['923165123719'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
